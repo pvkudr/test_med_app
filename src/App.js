4cefd3d79a9a14f36ad3ff7ef911d992cac37a42
pvkudr <pvkudr@gmail.com>
2024-07-29 11:31:55 +0800

    Add Instant Consultation logic

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 // Import necessary modules from React library
-import React, { useEffect } from 'react';
-import Landing_Page from './Components/Landing_Page/Landing_Page';
-import Login from './Components/Login/Login';
-import Sign_Up from './Components/SIgn_Up/Sign_Up';
-
+import React, { useEffect } from "react";
+import Landing_Page from "./Components/Landing_Page/Landing_Page";
+import Login from "./Components/Login/Login";
+import Sign_Up from "./Components/SIgn_Up/Sign_Up";
+import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
 
 // Import components for routing from react-router-dom library
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Import custom Navbar component
-import Navbar from './Components/Navbar/Navbar';
+import Navbar from "./Components/Navbar/Navbar";
 
 // Function component for the main App
 function App() {
-
   // Render the main App component
   return (
     <div className="App">
-        {/* Set up BrowserRouter for routing */}
-        <BrowserRouter>
-          {/* Display the Navbar component */}
-          <Navbar/>
+      {/* Set up BrowserRouter for routing */}
+      <BrowserRouter>
+        {/* Display the Navbar component */}
+        <Navbar />
 
-          {/* Set up the Routes for different pages */}
-          <Routes>
-            {/* Define individual Route components for different pages */}
-            <Route path="/" element={<Landing_Page/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/signup" element={<Sign_Up/>}/>
-          </Routes>
-        </BrowserRouter>
+        {/* Set up the Routes for different pages */}
+        <Routes>
+          {/* Define individual Route components for different pages */}
+          <Route path="/" element={<Landing_Page />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Sign_Up />} />
+          <Route
+            path="/instant-consultation"
+            element={<InstantConsultation />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
